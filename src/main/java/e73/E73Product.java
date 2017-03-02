34bdf76805bfd4ef67f76f06cdcf4b2b1d80abdf
nurulatiqah <nurulatiqahali@gmail.com>
2017-03-02 22:38:08 +0800

    first commit

--- src/main/java/e73/E73Product.java
+++ src/main/java/e73/E73Product.java
@@ -10,15 +10,18 @@ public class E73Product {
 	double priceAtStart;
 
 	public E73Product(String Banana, double p, int a) {
-		nameAtStart = Banana; // nameAtStart;
+		nameAtStart = Banana; //initialized
 		priceAtStart = 1.1;
 		amountAtStart = 13.0;
 	}
 
-	public void printProduct() { // initialized
-		nameAtStart = "Glister";
-		priceAtStart = 29.90;
-		amountAtStart = 2;
+	public void printProduct() { 
+//		nameAtStart = "Glister";
+//		priceAtStart = 29.90;
+//		amountAtStart = 2;
+		
+		System.out.println("Products " + nameAtStart + ", Price "
+				+ priceAtStart + ", Amount " + amountAtStart );
 	}
 
 }
--- src/main/java/e73/E73ProductMain.java
+++ src/main/java/e73/E73ProductMain.java
@@ -8,12 +8,6 @@ public class E73ProductMain {
 	public static void main(String[] args) {
 
 		E73Product products = new E73Product("Banana", 1.1, 13);
-
-		System.out.println("Products " + products.nameAtStart + " Amount " + products.amountAtStart + " Price "
-				+ products.priceAtStart);
-		System.out.println(" ");
 		products.printProduct();
-		System.out.println("Products " + products.nameAtStart + " Amount " + products.amountAtStart + " Price "
-				+ products.priceAtStart);
 	}
 }
